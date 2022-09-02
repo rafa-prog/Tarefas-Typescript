64fb0e304b63714c9d7145bbf6ee5470e3de6998
Rafael <72702631+rafa-prog@users.noreply.github.com>
2022-09-02 13:06:07 -0300

    AppProdutos

---
+++ Atividades/AppProduto/src/app/app-routing.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+
+const routes: Routes = [
+  {
+    path: 'home',
+    loadChildren: () => import('./pages/home/home.module').then( m => m.HomePageModule)
+  },
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full'
+  },
+  {
+    path: 'cadastro',
+    loadChildren: () => import('./pages/cadastro/cadastro.module').then( m => m.CadastroPageModule)
+  },
+  {
+    path: 'detalhes',
+    loadChildren: () => import('./pages/detalhes/detalhes.module').then( m => m.DetalhesPageModule)
+  },
+];
+
+@NgModule({
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })
+  ],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ Atividades/AppProduto/src/app/models/produto.ts
@@ -0,0 +1,14 @@
+import { Provedor } from "./provedor"
+
+export class Produto {
+    id: string
+
+    nome: string
+    provedor: Provedor
+    info: string
+    preco: number
+    retornoProvedor: number
+    dataCompra: string
+
+
+}
